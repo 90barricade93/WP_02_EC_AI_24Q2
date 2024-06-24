0696bc8ea443ba327ffe96db12f9becda7c4f0cf
Raymond de Vries <raymonddevries76@gmail.com>
2024-06-24 14:56:23 +0200

    fixed a problem with selection from hamburger menus

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import { useState } from 'react';
 import { JokeGenerator } from '@/components/joke-generator'; 
 import { useChat } from "ai/react";
-function ComingSoonPage() {
+function JokePage() {
   return (
     <div className="h-screen w-screen bg-black text-white flex justify-center items-center">
       <JokeGenerator />
@@ -12,4 +12,4 @@ function ComingSoonPage() {
   );
 }
 
-export default ComingSoonPage;
+export default JokePage;
